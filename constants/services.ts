566229ec7357d1e8364057c83c93a7f0bcb48947
fallen02 <000.sb13@gmail.com>
2025-09-07 12:07:01 +0530

    fedora

---
+++ constants/services.ts
@@ -0,0 +1,26 @@
+export const Services = [
+  {
+    title: "We don't just design, we ",
+    colordTitle: "unleash creative",
+    restTitle: "firestorms",
+    desc: "Tired of bland marketing and cookie-cutter websites? We infuse your brand with bold ideas and strategic brilliance, igniting campaigns that capture hearts and drive tangible results. Forget sparklers, we're here to light galaxies on fire.",
+  },
+  {
+    title: "We ",
+    colordTitle: "fuel creativity",
+    restTitle: "with data, guide passion with precision",
+    desc: "We're not just artistic dreamers, we're strategic alchemists. We blend data-driven insights with unbridled passion, ensuring your campaigns reach the right audience, hit the right chords, and deliver measurable results. Because passion without direction is a beautiful mess.",
+  },
+  {
+    title: "We craft ",
+    colordTitle: "emotional connections",
+    restTitle: ", not just visuals and copy",
+    desc: "We delve deeper than pixels and prose. We understand the human heart, the language of emotions. We weave stories that resonate, visuals that linger in minds, and content that sparks conversations. Because true connection is the foundation of brand loyalty.",
+  },
+  {
+    title: "We don't just work with you, we become your ",
+    colordTitle: "creative champions",
+    restTitle: "",
+    desc: "We don't see clients, we see collaborators. We believe in building partnerships, in understanding your vision, and becoming an extension of your team. Your goals are our canvas, your success our masterpiece. We paint your dreams into reality, together.",
+  },
+];
